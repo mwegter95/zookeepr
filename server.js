d29e6d1f69d18c820fc4e923ccd58d8053a527b9
hitmaker <mwegter95@gmail.com>
2021-12-16 23:10:16 -0600

    added POST functionality

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -62,8 +67,14 @@ app.get('/api/animals/:id', (req, res) => {
     res.json(result);
   } else {
     res.sendStatus(404);
-  }
-  });
+  };
+});
+
+app.post('/api/animals', (req, res) => {
+  // req.body is where our incoming content will be
+  console.log(req.body);
+  res.json(req.body);
+});
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
